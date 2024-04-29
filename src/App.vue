1e51d23057f8883018fffb6bae6a141bcc2108f4
Gast <gast@192.168.1.26>
2024-04-29 21:24:27 +0200

    Removed todo list

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,4 @@
 <script setup>
-// TO DO:
-// [X] Kleuren & font
-// [X] Add active link
-// [X] Voeg muis plaatje toe naast naam
-// [X] Ruimte tussen nav en rest
-// [ ] hover
-// [X] Style de buttons
-// [X] Show the list of meals at the proper time
-// [ ] Check if all things indeed need to be async
-// [X] Create a card for the recipes
-// [X] Change checkbox to select
-// [X] Style the page
-// [X] Add a few standards tasks to the calendar
-// [ ] Add 2 unit test suites
-// [X] eslint check
-// [X] deploy on Github pages
-
 import { ref } from "vue";
 
 const navItems = ref([
